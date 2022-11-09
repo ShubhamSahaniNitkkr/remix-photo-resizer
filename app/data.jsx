d296f53fa3aeccb85684533cc442c89c26a8e333
ShubhamSunny <shubhamsahaninitkkr@gmail.com>
2022-11-09 21:57:14 +0530

    -code splitting - reset button feature

--- app/data.jsx
+++ app/data.jsx
@@ -29,11 +29,11 @@ const profilePicGuide = [
 ];
 
 const sliderMarks = [
-  { value: 20, label: "20" },
-  { value: 40, label: "40" },
-  { value: 60, label: "60" },
-  { value: 80, label: "80" },
-  { value: 100, label: "100" },
+  { value: 20, label: "" },
+  { value: 40, label: "" },
+  { value: 60, label: "" },
+  { value: 80, label: "" },
+  { value: 100, label: "" },
 ];
 
 export { userInfo, brandName, profilePicGuide, sliderMarks };
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -8,12 +8,12 @@ import {
   Image,
   Flex,
   createStyles,
-  Title,
   Slider,
   FileButton,
 } from "@mantine/core";
 import Cropper from "react-easy-crop";
 import {
+  IconRotateDot,
   IconCloudUpload,
   IconDownload,
   IconCamera,
@@ -26,7 +26,7 @@ import { profilePicGuide, sliderMarks } from "../data";
 const useStyles = createStyles((theme, _params) => {
   return {
     cardBody: {
-      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      padding: `${theme.spacing.xs}px 0px`,
       marginBottom: "5px",
     },
     btn: {
@@ -55,7 +55,7 @@ const useStyles = createStyles((theme, _params) => {
       justifyContent: "center",
       alignItems: "center",
       color: "#7e75f2",
-      minHeight: "70vh",
+      height: "40vh",
     },
     cropperBox: {
       height: "60vh",
@@ -76,7 +76,6 @@ const Home = () => {
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
     setCroppedArea(croppedAreaPixels);
   }, []);
-
   const onChooseImage = (file) => {
     if (file) {
       const reader = new FileReader();
@@ -87,147 +86,17 @@ const Home = () => {
     }
   };
 
+  const resetFn = () => {
+    setImage(null);
+    setCroppedArea(null);
+    setCrop({ x: 0, y: 0 });
+    setZoom(1);
+  };
+
   if (typeof window === "undefined") return null;
 
-  return (
-    <Grid>
-      <Grid.Col span={8}>
-        <Card shadow="sm" p="lg" radius="md" withBorder>
-          <Card.Section withBorder inheritPadding py="xs">
-            <Flex justify="space-between" align="center" direction="row">
-              <div>
-                <Group position="apart">
-                  <Text weight={800} size="xl">
-                    Profile picture
-                  </Text>
-                </Group>
-                <Group position="apart">
-                  <Text weight={200} color="gray">
-                    Capture or choose a picture from your device.
-                  </Text>
-                </Group>
-              </div>
-            </Flex>
-          </Card.Section>
-          <Card.Section className={classes.cardBody}>
-            <div className="container-cropper">
-              {image ? (
-                <>
-                  <div className={cx(classes.cropperBox, "cropper")}>
-                    <Cropper
-                      image={image}
-                      crop={crop}
-                      zoom={zoom}
-                      aspect={1}
-                      onCropChange={setCrop}
-                      onZoomChange={setZoom}
-                      onCropComplete={onCropComplete}
-                    />
-                    <br />
-                  </div>
-                </>
-              ) : (
-                <Image
-                  height="60vh"
-                  src="./assets/no-image.jpg"
-                  alt="no-image"
-                />
-              )}
-            </div>
-            <br />
-            <div className={classes.sliderBox}>
-              <Slider
-                thumbSize={20}
-                defaultValue={1}
-                color="violet"
-                step={1}
-                marks={sliderMarks}
-                styles={(theme) => ({
-                  markLabel: { color: "#7e75f2", fontWeight: "bold" },
-                })}
-                onChange={(zoom) => {
-                  setZoom(zoom / 10);
-                }}
-              />
-            </div>
-            <br />
-          </Card.Section>
-
-          <Flex justify="space-between" align="center" direction="row">
-            <Flex
-              justify="space-between"
-              align="center"
-              direction="row"
-              gap={10}
-            >
-              <Button
-                variant="filled"
-                color="violet"
-                className={classes.btn}
-                size="sm"
-                radius="xl"
-                leftIcon={<IconCamera size={20} />}
-              >
-                Capture picture
-              </Button>
-              <FileButton
-                onChange={setImage}
-                accept="image/*"
-                onChange={onChooseImage}
-              >
-                {(props) => (
-                  <Button
-                    variant="filled"
-                    color={image ? "green" : "violet"}
-                    className={classes.btn}
-                    size="sm"
-                    radius="xl"
-                    leftIcon={<IconPhoto size={20} />}
-                    {...props}
-                  >
-                    {image ? "Picture selected" : "Choose Picture"}
-                  </Button>
-                )}
-              </FileButton>
-            </Flex>
-            <Flex
-              justify="space-between"
-              align="center"
-              direction="row"
-              gap={10}
-            >
-              <Button
-                variant="filled"
-                color="violet"
-                className={classes.btn}
-                size="sm"
-                radius="xl"
-                leftIcon={<IconDownload size={20} />}
-                disabled={!image}
-                title={
-                  image
-                    ? "Click to download image"
-                    : "Please click or upload image first"
-                }
-                onClick={() => generateDownload(image, croppedArea)}
-              >
-                Download
-              </Button>
-              <Button
-                variant="filled"
-                color="violet"
-                className={classes.btn}
-                size="sm"
-                radius="xl"
-                leftIcon={<IconCloudUpload size={20} />}
-                disabled={!image}
-              >
-                Upload
-              </Button>
-            </Flex>
-          </Flex>
-        </Card>
-      </Grid.Col>
+  const renderGuideCard = () => {
+    return (
       <Grid.Col span={4}>
         <Card withBorder shadow="sm" radius="md">
           <Card.Section withBorder inheritPadding py="xs">
@@ -251,6 +120,184 @@ const Home = () => {
           </Card.Section>
         </Card>
       </Grid.Col>
+    );
+  };
+
+  const renderActionBtns = () => {
+    return (
+      <Flex justify="space-between" align="center" direction="row">
+        <Flex justify="space-between" align="center" direction="row" gap={10}>
+          <Button
+            variant="filled"
+            color="violet"
+            className={classes.btn}
+            size="sm"
+            radius="xl"
+            leftIcon={<IconCamera size={20} />}
+          >
+            Capture picture
+          </Button>
+          <FileButton
+            onChange={setImage}
+            accept="image/*"
+            onChange={onChooseImage}
+          >
+            {(props) => (
+              <Button
+                variant="filled"
+                color={image ? "green" : "violet"}
+                className={classes.btn}
+                size="sm"
+                radius="xl"
+                leftIcon={<IconPhoto size={20} />}
+                {...props}
+              >
+                {image ? "Picture selected" : "Choose Picture"}
+              </Button>
+            )}
+          </FileButton>
+        </Flex>
+        <Flex justify="space-between" align="center" direction="row" gap={10}>
+          <Button
+            variant="filled"
+            color="violet"
+            className={classes.btn}
+            size="sm"
+            radius="xl"
+            leftIcon={<IconDownload size={20} />}
+            disabled={!image}
+            title={
+              image
+                ? "Click to download image"
+                : "Please click or upload image first"
+            }
+            onClick={() => generateDownload(image, croppedArea)}
+          >
+            Download
+          </Button>
+          <Button
+            variant="filled"
+            color="violet"
+            className={classes.btn}
+            size="sm"
+            radius="xl"
+            leftIcon={<IconCloudUpload size={20} />}
+            disabled={!image}
+          >
+            Upload
+          </Button>
+        </Flex>
+      </Flex>
+    );
+  };
+
+  const renderCropper = () => {
+    return (
+      <div className="container-cropper">
+        {image ? (
+          <>
+            <div className={cx(classes.cropperBox, "cropper")}>
+              <Cropper
+                image={image}
+                crop={crop}
+                zoom={zoom}
+                aspect={1}
+                onCropChange={setCrop}
+                onZoomChange={setZoom}
+                onCropComplete={onCropComplete}
+                style={{
+                  height: "30vh",
+                }}
+              />
+              <br />
+            </div>
+          </>
+        ) : (
+          <Image height="60vh" src="./assets/no-image.jpg" alt="no-image" />
+        )}
+      </div>
+    );
+  };
+
+  const renderCropperContainer = () => {
+    return (
+      <>
+        <Card shadow="sm" p="lg" radius="xs">
+          <Card.Section className={classes.cardBody}>
+            {renderCropper()}
+            <br />
+            {renderSlider()}
+          </Card.Section>
+        </Card>
+        <br />
+      </>
+    );
+  };
+
+  const renderSlider = () => {
+    return (
+      <div className={classes.sliderBox}>
+        <Slider
+          disabled={!image}
+          thumbSize={20}
+          defaultValue={1}
+          color="violet"
+          step={1}
+          marks={sliderMarks}
+          styles={(theme) => ({
+            markLabel: { color: "#7e75f2", fontWeight: "bold" },
+          })}
+          onChange={(zoom) => {
+            setZoom(zoom / 10);
+          }}
+        />
+      </div>
+    );
+  };
+
+  const renderCardHeadings = () => {
+    return (
+      <Card.Section withBorder inheritPadding py="xs">
+        <Flex justify="space-between" align="center" direction="row">
+          <div>
+            <Group position="apart">
+              <Text weight={800} size="xl">
+                Profile picture
+              </Text>
+            </Group>
+            <Group position="apart">
+              <Text weight={200} color="gray">
+                Capture or choose a picture from your device.
+              </Text>
+            </Group>
+          </div>
+          <Button
+            variant="filled"
+            color="violet"
+            className={classes.btn}
+            size="sm"
+            radius="xl"
+            leftIcon={<IconRotateDot size={20} />}
+            disabled={!image}
+            onClick={() => resetFn()}
+          >
+            Reset
+          </Button>
+        </Flex>
+      </Card.Section>
+    );
+  };
+
+  return (
+    <Grid>
+      <Grid.Col span={8}>
+        <Card shadow="sm" p="lg" radius="md" withBorder>
+          {renderCardHeadings()}
+          {renderCropperContainer()}
+          {renderActionBtns()}
+        </Card>
+      </Grid.Col>
+      {renderGuideCard()}
     </Grid>
   );
 };
